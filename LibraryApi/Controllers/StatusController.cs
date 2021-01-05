31da2820ad320f8a4027f62f97f488c14f3fe123
Jeffry Gonzalez <jeff@hypertheory.com>
2021-01-05 15:57:56 -0500

    Add project files.

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        
+        // GET /status
+        [HttpGet("status")]
+        public ActionResult<GetStatusResponse> GetTheStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                Message = "Looks Good",
+                LastChecked = DateTime.Now
+            };
+           
+            return Ok(response);
+        }
+
+        // GET /sayhi/Putintane
+        [HttpGet("sayhi/{name}")]
+        public ActionResult SayHello(string name)
+        {
+            return Ok($"Hello, {name}!");
+        }
+
+        [HttpGet("orders/{year:int}/{month:int:range(1,12)}/{day:int}")]
+        public ActionResult GetOrdersFor(int year, int month, int day)
+        {
+            return Ok($"Getting orders for {year}/{month}/{day}");
+        }
+
+        // Query Strings
+        [HttpGet("employees")]
+        public ActionResult GetEmployees([FromQuery] string department = "All")
+        {
+            return Ok($"Showing employees in department {department}");
+
+        }
+    }
+
+
+    // Method Description (Request | Response) Requests to the server, REsponses come from the server
+    public class GetStatusResponse
+    {
+        public string Message { get; set; }
+        public DateTime LastChecked { get; set; }
+    }
+}
---
+++ LibraryApi/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Library API for BES 100",
+                    Version = "1.0",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    {
+                        Name="Jeff Gonzalez",
+                        Email="jeff@hypertheory.com"
+                    },
+                    Description = "This is for Training"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API");
+                c.RoutePrefix = "";
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
